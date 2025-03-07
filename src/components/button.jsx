8250be1cbb0ac4dc854d0479f903ab5178f1ec83
unknown <lakshmi7708671565@gmail.com>
2025-03-07 09:48:39 +0530

    Assignment Completed

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,13 @@
 // write button card here
+import React from 'react';
+import '../App.css'
+
+const Button = () => {
+    return (
+        <div>
+            <button>View Product</button>
+        </div>
+    );
+}
+
+export default Button;
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,28 @@
 // write product card here
+import React from 'react';
+import Button from "./button.jsx"
+import productImage from "../assets/productImage.jpg"
+import "../App.css"
+
+const Productcard = () => {
+    return (
+        <>
+            <div className='productCard'>
+                <div className='productImg'>
+                    <img src={productImage} alt='Product' className='image'/>
+                </div>
+                <div className='productName'>
+                    <p>Apple Laptop Ultra</p>
+                </div>
+                <div className='price'>
+                    <p>Rs: 500</p>
+                </div>
+                <div>
+                    <Button />
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Productcard;
